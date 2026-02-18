67b283084f6345fb7d00c428f0475fd106c4a2f5
eledascanio <elenasofia.dascanio@studenti.unicam.it>
2026-02-18 21:35:24 +0100

    classe Invito.java

--- src/main/java/it/unicam/ids/model/Invito.java
+++ src/main/java/it/unicam/ids/model/Invito.java
@@ -1,14 +1,26 @@
 package it.unicam.ids.model;
 
-import java.time.LocalDate;
-import java.util.Objects;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id", "teamId", "destinatario"})
+@ToString
 public class Invito {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate dataInvio;
     private LocalDate dataRisposta;
+
+    @Enumerated(EnumType.STRING)
     private StatoInvito stato;
+
     private Long teamId;
     private Long destinatario;
 
@@ -22,50 +34,5 @@ public class Invito {
         this.stato = StatoInvito.IN_ATTESA;
         this.teamId = teamId;
         this.destinatario = destinatario;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public LocalDate getDataInvio() { return dataInvio; }
-    public void setDataInvio(LocalDate dataInvio) { this.dataInvio = dataInvio; }
-
-    public LocalDate getDataRisposta() { return dataRisposta; }
-    public void setDataRisposta(LocalDate dataRisposta) { this.dataRisposta = dataRisposta; }
-
-    public StatoInvito getStato() { return stato; }
-    public void setStato(StatoInvito stato) { this.stato = stato; }
-
-    public Long getTeamId() { return teamId; }
-    public void setTeamId(Long teamId) { this.teamId = teamId; }
-
-    public Long getDestinatario() { return destinatario; }
-    public void setDestinatario(Long destinatario) { this.destinatario = destinatario; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Invito invito = (Invito) o;
-        return Objects.equals(id, invito.id) &&
-                Objects.equals(teamId, invito.teamId) &&
-                Objects.equals(destinatario, invito.destinatario);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, teamId, destinatario);
-    }
-
-    @Override
-    public String toString() {
-        return "Invito{" +
-                "id=" + id +
-                ", dataInvio=" + dataInvio +
-                ", dataRisposta=" + dataRisposta +
-                ", stato=" + stato +
-                ", teamId=" + teamId +
-                ", destinatarioId=" + destinatario +
-                '}';
     }
 }
