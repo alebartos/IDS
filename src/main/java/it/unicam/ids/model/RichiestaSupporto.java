696ddb09d6018744ec52925b29682e7c6ebf1a57
alebartos <alebartolini02@gmail.com>
2026-02-18 21:45:00 +0100

    fatto il porting a springboot di RichiestaSupporto.java

--- src/main/java/it/unicam/ids/model/RichiestaSupporto.java
+++ src/main/java/it/unicam/ids/model/RichiestaSupporto.java
@@ -1,10 +1,19 @@
 package it.unicam.ids.model;
 
-import java.time.LocalDate;
-import java.util.Objects;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id"})
+@ToString
 public class RichiestaSupporto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String descrizione;
     private Long teamId;
@@ -23,48 +32,5 @@ public class RichiestaSupporto {
         this.hackathonId = hackathonId;
         this.data = LocalDate.now();
         this.risolta = false;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getDescrizione() { return descrizione; }
-    public void setDescrizione(String descrizione) { this.descrizione = descrizione; }
-
-    public Long getTeamId() { return teamId; }
-    public void setTeamId(Long teamId) { this.teamId = teamId; }
-
-    public LocalDate getData() { return data; }
-    public void setData(LocalDate data) { this.data = data; }
-
-    public Long getHackathonId() { return hackathonId; }
-    public void setHackathonId(Long hackathonId) { this.hackathonId = hackathonId; }
-
-    public boolean isRisolta() { return risolta; }
-    public void setRisolta(boolean risolta) { this.risolta = risolta; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RichiestaSupporto that = (RichiestaSupporto) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "RichiestaSupporto{" +
-                "id=" + id +
-                ", descrizione='" + descrizione + '\'' +
-                ", teamId=" + teamId +
-                ", data=" + data +
-                ", hackathonId=" + hackathonId +
-                ", risolta=" + risolta +
-                '}';
     }
 }
