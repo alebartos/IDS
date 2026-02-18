a2c1821e5218ef56f91ac48901ca01f89dc1933a
eledascanio <elenasofia.dascanio@studenti.unicam.it>
2026-02-18 21:48:10 +0100

    modificate classi per Springboot

--- src/main/java/it/unicam/ids/dto/DatiProgetto.java
+++ src/main/java/it/unicam/ids/dto/DatiProgetto.java
@@ -1,44 +1,16 @@
 package it.unicam.ids.dto;
 
-/**
- * DTO per i dati di un progetto da sottomettere.
- */
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DatiProgetto {
 
     private String titolo;
     private String descrizione;
     private String linkRepository;
-
-    public DatiProgetto() {
-    }
-
-    public DatiProgetto(String titolo, String descrizione, String linkRepository) {
-        this.titolo = titolo;
-        this.descrizione = descrizione;
-        this.linkRepository = linkRepository;
-    }
-
-    public String getTitolo() {
-        return titolo;
-    }
-
-    public void setTitolo(String titolo) {
-        this.titolo = titolo;
-    }
-
-    public String getDescrizione() {
-        return descrizione;
-    }
-
-    public void setDescrizione(String descrizione) {
-        this.descrizione = descrizione;
-    }
-
-    public String getLinkRepository() {
-        return linkRepository;
-    }
-
-    public void setLinkRepository(String linkRepository) {
-        this.linkRepository = linkRepository;
-    }
 }
