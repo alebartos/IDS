d635499fade9f1bf9b8df5429e5ec3a00c2aa976
eledascanio <elenasofia.dascanio@studenti.unicam.it>
2026-02-19 18:17:03 +0100

    correzioni errori

--- src/main/java/it/unicam/ids/service/ValutazioneService.java
+++ src/main/java/it/unicam/ids/service/ValutazioneService.java
@@ -10,6 +10,7 @@ import it.unicam.ids.repository.SottomissioneRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import java.time.LocalDate;
 
 @Service
